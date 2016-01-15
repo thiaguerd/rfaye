9fa7a411f20d83f9adeb8c74ffd4644b3e09d536
Thiago Feitosa <mail@thiago.pro>
2016-01-15 18:14:25 -0500

    try cath on call rfaye extension

--- lib/rfaye/faye_extension.rb
+++ lib/rfaye/faye_extension.rb
@@ -1,16 +1,22 @@
 module Rfaye
 	class FayeExtension
 		def incoming(message, callback)
-			p = Rfaye::Conf[:secure_channel_prefix].start_with?("/") ? Rfaye::Conf[:secure_channel_prefix] : "/" + Rfaye::Conf[:secure_channel_prefix]
-			if message["channel"].start_with? p
-				if !token_check(message)
-					message["error"] = "channel private!"
-					puts "BLOCK #{message}" if Rfaye::Conf[:debug]
+			begin
+				p = Rfaye::Conf[:secure_channel_prefix].start_with?("/") ? Rfaye::Conf[:secure_channel_prefix] : "/" + Rfaye::Conf[:secure_channel_prefix]
+				if message["channel"].start_with? p
+					if !token_check(message)
+						message["error"] = "channel private!"
+						puts "BLOCK #{message}" if Rfaye::Conf[:debug]
+					end
+				else
+					puts "OK #{message}" if Rfaye::Conf[:debug]
 				end
-			else
-				puts "OK #{message}" if Rfaye::Conf[:debug]
+				callback.call(message)
+			rescue
+				message = {}
+				message["error"] = "request is not valid"
+				callback.call(message)
 			end
-			callback.call(message)
 		end
 		private
 			def token_check message
